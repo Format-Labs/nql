3ce75aad8e4d7ef6a4e4694c8d34bdbd1c66774c
kelcheone <kelcheone@gmail.com>
2022-07-03 22:39:52 +0300

    🛠: query & sync"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QueryController } from './query/query.controller';
+import { Query } from './query';
+import { SyncController } from './sync/sync.controller';
+import { Sync } from './sync';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, QueryController, SyncController],
+  providers: [AppService, Query, Sync],
 })
 export class AppModule {}
